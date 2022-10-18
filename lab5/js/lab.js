7be351b0ed4787619ead496df1a30a9efa36e327
GAAQUINO1 <114959362+GAAQUINO1@users.noreply.github.com>
2022-10-17 16:17:26 -0800

    completed lab 5 with css edits, java files made and displayed

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:   Angel A
+ * Created:   10/17/22
+ * license: Public Domain
+ **/
+
+ //Define Variables
+ make = "Ford";
+ model = "Bronco";
+ color = "Blue";
+ year = 1971;
+
+ //calculate
+ age = 2022 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
