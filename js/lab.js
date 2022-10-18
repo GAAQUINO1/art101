fad97e0f5c0aeb48ee70a6f78be0056330ae2972
GAAQUINO1 <114959362+GAAQUINO1@users.noreply.github.com>
2022-10-17 16:23:52 -0800

    fixed messed up code

---
+++ js/lab.js
@@ -0,0 +1,16 @@
+//summary.
+//description
+//@link URL
+//@file
+//@author
+//@since 10/12/22
+
+
+
+//create a chess game
+//Print out each side’s pieces
+//Accept starting player input
+// validate user move
+//Evaluate possible moves
+//Choose computer move
+//Continue until one side is victorious/draws
