f3b9b01a8d63ba2c045f1f3e1c72a66376702c36
GAAQUINO1 <114959362+GAAQUINO1@users.noreply.github.com>
2022-11-16 20:37:34 -0800

    created lab14

---
+++ lab14/js/lab.js
@@ -0,0 +1,10 @@
+/**
+ * Author:   Angel A
+ * Created:   11/14/22
+ * license: Public Domain
+ **/
+
+ const output = document.getElementById("output");
+ const p = document.createElement("p");
+ p.innerHTML = "Debugging...."
+ output.appendChild(p);
